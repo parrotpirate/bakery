f7ca4b7aa881af143ab14dd4fc57f7d2b44bb24c
Stephen Parker <parrotpirate@gmail.com>
2016-01-26 19:25:04 -0500

    Recipe slider animation/window resize event fix
    Added animation to recipe selector slider using Slick plugin.  Also
    fixed window resize event not firing on window snapping or maximize
    button by adding a timeout to resize function.

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,30 @@ jQuery(document).ready(function($) {
 		$('#recipes .recipe_card .recipe_selector .selector_window .selector_img img').css({'height':ar+'px'});  
 	}
 
-	function leftSelector() {
-		$('.selector_img:first-child').appendTo('.selector_window');
-	}
-
-	function rightSelector() {
-		$('.selector_img:last-child').prependTo('.selector_window');
-	}
+	
 
 	/*Image resize on page load*/
 	imgAR();
 	/*Image resize on page resize*/
-	$(window).resize(imgAR);
+	$(window).resize(function () {
+		 setTimeout(imgAR, 300);
+	});
 
-	$('.left.selector').click(leftSelector);
-	$('.right.selector').click(rightSelector);
+	/*Slick scrolling carousel*/
+	$('.selector_window').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		prevArrow: $('.right.selector'),
+		nextArrow: $('.left.selector'),
+		draggable: false,
+		swipe: false,
+		touchMove: false
+	});
+
+	$('.selector_img').click(function(e) {
+		e.preventDefault();
+	});
 
 
 });
